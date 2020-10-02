3e69d233199f3d895061228cfe5494fef9c2d0c0
2692968652@qq.com <2692968652@qq.com>
2020-10-02 10:56:21 +0800

    add 10.2

---
+++ build/precache-manifest.04e4771fe932c69540eaa38d9c486bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d8d648e453c2fbb6455f80ea0f7511",
+    "url": "./index.html"
+  },
+  {
+    "revision": "22cbfe10e1ac7ab466d5",
+    "url": "./static/css/main.886b93db.chunk.css"
+  },
+  {
+    "revision": "a8e18be21d64f44a39ad",
+    "url": "./static/js/2.c5dd38ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c5dd38ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22cbfe10e1ac7ab466d5",
+    "url": "./static/js/main.0ff2f05f.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
