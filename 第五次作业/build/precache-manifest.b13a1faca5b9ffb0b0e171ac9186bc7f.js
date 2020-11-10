aabd89c3f07b22de2170b9d7b29bd09cd3f331c5
2692968652@qq.com <2692968652@qq.com>
2020-11-10 15:38:08 +0800

    work-5 taobao

---
+++ 第五次作业/build/precache-manifest.b13a1faca5b9ffb0b0e171ac9186bc7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31598889a13383b82c108721af5bd2eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ce84e4225680a02745bd",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "3770725032e568d06464",
+    "url": "./static/css/main.a368c7fe.chunk.css"
+  },
+  {
+    "revision": "ce84e4225680a02745bd",
+    "url": "./static/js/2.f93d01f9.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.f93d01f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3770725032e568d06464",
+    "url": "./static/js/main.51399898.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
